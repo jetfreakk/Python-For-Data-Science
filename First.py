578298dcfde59cf0510ae366b24387574c362583
jetfreakk <33203722+jetfreakk@users.noreply.github.com>
2019-07-23 08:46:32 +0530

    Reading a CSV File

---
+++ First.py
@@ -0,0 +1,7 @@
+import numpy as np 
+import scipy as sp 
+import pandas as pd 
+import matplotlib as mpl 
+import seaborn as sns
+df = pd.read_csv("F:/Github/Python for Data Science/data.csv")
+df.head()
