3c7f5230559f2265980f065303b55c7e7c3bd642
jetfreakk <33203722+jetfreakk@users.noreply.github.com>
2019-07-23 09:06:54 +0530

    Data Type of Columns

--- First.py
+++ First.py
@@ -4,5 +4,8 @@ import pandas as pd
 import matplotlib as mpl 
 import seaborn as sns
 df = pd.read_csv("F:/Github/Python for Data Science/KB.csv")
-df.head()
+df.head(10)
 df.tail(10)
+df['ImageId'].dtype
+df['NameOfPokemon'].dtype
+df.dtypes
