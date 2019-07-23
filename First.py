14fd0090e04fd9ed48159d935ac2adf9865080de
jetfreakk <33203722+jetfreakk@users.noreply.github.com>
2019-07-23 09:01:31 +0530

    Reading N Rows

--- First.py
+++ First.py
@@ -3,5 +3,6 @@ import scipy as sp
 import pandas as pd 
 import matplotlib as mpl 
 import seaborn as sns
-df = pd.read_csv("F:/Github/Python for Data Science/data.csv")
+df = pd.read_csv("F:/Github/Python for Data Science/KB.csv")
 df.head()
+df.tail(10)
